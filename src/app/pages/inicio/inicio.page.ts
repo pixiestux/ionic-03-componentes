5da0e20d81f97feb6e75e54f764a900e65251884
Jose Luis <joseluis@MacBook-Pro-de-Jose.local>
2021-01-27 17:53:57 -0600

    ion avatar, ion chip

--- src/app/pages/inicio/inicio.page.ts
+++ src/app/pages/inicio/inicio.page.ts
@@ -24,6 +24,11 @@ export class InicioPage implements OnInit {
       name: "Alert",
       redirectTo: "/alert"
     },
+    {
+      icon: "beaker",
+      name: "Avatar",
+      redirectTo: "/avatar"
+    },
 ]
 
   constructor() { }
